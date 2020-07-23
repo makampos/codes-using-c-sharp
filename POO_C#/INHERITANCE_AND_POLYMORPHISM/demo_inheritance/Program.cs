1815ce18c4f5152715493386711035c28c199fc5
Matheus de Campos <matheus.campos@edu.unifil.br>
2020-07-23 15:25:12 -0300

    update

---
+++ POO_C#/INHERITANCE_AND_POLYMORPHISM/demo_inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using demo_inheritance.Entities;
+
+namespace demo_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010 ,"Bob Brow", 100.0, 500.0);
+
+            System.Console.WriteLine(account.Balance);
+
+            // account.Balance = 200;
+        }
+    }
+}
