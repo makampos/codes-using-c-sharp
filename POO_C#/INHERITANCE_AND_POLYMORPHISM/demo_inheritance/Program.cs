540ba63ee53d1f6a3a42eaf254bfaa9009d8da9a
Matheus de Campos <matheus.campos@edu.unifil.br>
2020-07-23 15:48:39 -0300

    update

--- POO_C#/INHERITANCE_AND_POLYMORPHISM/demo_inheritance/Program.cs
+++ POO_C#/INHERITANCE_AND_POLYMORPHISM/demo_inheritance/Program.cs
@@ -7,11 +7,37 @@ namespace demo_inheritance
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010 ,"Bob Brow", 100.0, 500.0);
+           Account acc = new Account(1001, "Alex", 0.0);
+           BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500);
 
-            System.Console.WriteLine(account.Balance);
+           // UPCASTING
 
-            // account.Balance = 200;
+           Account acc1 = bacc;
+           Account acc2 = new BusinessAccount(1003, "Bob", 0.0 ,200.0);
+           Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+
+           // DOWNCASTING 
+
+           BusinessAccount acc4 = (BusinessAccount)acc2;
+           acc4.Loan(100.0);
+
+           //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+           if(acc3 is BusinessAccount) {
+             // Other syntax
+            //  BusinessAccount acc5 = (BusinessAccount)acc3;
+               BusinessAccount acc5 = acc3 as BusinessAccount;
+               acc5.Loan(200.0);
+               System.Console.WriteLine("Loan!");
+           }
+
+           if(acc3 is SavingsAccount) 
+           {
+               SavingsAccount acc5 = (SavingsAccount)acc3;
+               acc5.UpdateBalance();
+               System.Console.WriteLine("Update!");
+           }
         }
     }
 }
