50d18d5a33218b1e390102404b6bf7245abda804
makampos <matheus.campos@edu.unifil.br>
2020-07-30 14:07:33 -0300

    update

---
+++ GENERICS_SET_DICTIONARY/generics_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace generics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            System.Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            System.Console.WriteLine();
+            System.Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
