1efa114f057ebe377bd9345bad91a0a55135f91b
makampos <matheus.campos@edu.unifil.br>
2020-07-30 15:11:21 -0300

    update

---
+++ GENERICS_SET_DICTIONARY/generics_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using generics_2.Entities;
+using generics_2.Services;
+
+namespace generics_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            System.Console.WriteLine("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);                
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            System.Console.Write("Max: ");
+            System.Console.Write(max);
+            System.Console.WriteLine();
+        }
+    }
+}
